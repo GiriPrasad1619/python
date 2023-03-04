43052942b948afa51727ed790b15dae6b568d5cd
GiriPrasad1619 <126935422+GiriPrasad1619@users.noreply.github.com>
2023-03-04 10:07:23 +0530

    Add files via upload

---
+++ leap year.py
@@ -0,0 +1,9 @@
+arr=[]
+n=int(input("enter the size"))
+for i in range(n):
+    value=int(input("Enter the values"))
+    arr.append(value)
+print("the given unsorted array")
+print(arr)
+small=(min(arr))    
+large=(max(arr))
