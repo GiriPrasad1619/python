7945fef08c9423f933a0dd2adedc31fef49cdbf2
GiriPrasad1619 <126935422+GiriPrasad1619@users.noreply.github.com>
2023-03-04 12:20:28 +0530

    Add files via upload

---
+++ average of three numbers.py
@@ -0,0 +1,8 @@
+Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+== RESTART: C:/Users/student/Documents/python lab/average of three numbers.py ==
+1st number5
+2st number5
+3st number5
+the average valude is  5.0
